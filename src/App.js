cdec9c059751d891fa6aef08e5d931d7bd1ab1ee
Eltayib <alltayeb112003@gmail.com>
2024-08-04 18:53:41 +0400

    setup gh-pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
 function App() {
+  const [showButton, setShowButton] = useState(true);
+
+  const toggleButton = () => {
+    setShowButton(!showButton);
+  };
+  const alering = () => {
+    alert("Thank You <3 💕")
+  } 
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex justify-center items-center font-mono font-bold text-3xl text-center min-h-screen">
+        <h1 className="text-3xl border-solid border-2 border-sky-500 rounded-2xl p-3"> Alya do you Forgive me ?🥺</h1>
+        <button className="mx-10" onClick={alering}>YES</button>
+        {showButton && <button  className="mx-10" onClick={toggleButton}>No</button>}
+        </div>  
     </div>
   );
 }
